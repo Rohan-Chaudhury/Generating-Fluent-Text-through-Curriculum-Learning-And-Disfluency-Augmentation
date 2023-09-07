eb213e4cf09a9d813ac5f2aa82e5d69a543ac205
Maria Teleki <mariateleki@csce-caverlee-s1.engr.tamu.edu>
2023-09-07 13:14:00 -0500

    code cleanup

--- disfluency-model/training_code/train.py
+++ disfluency-model/training_code/train.py
@@ -1,73 +1,65 @@
-
-
-
-BATCH_SIZE = 4
-
-model_name = "/home/grads/r/rohan.chaudhury/Disfluency/models/checkpoint-2253"
-print ("Model name: ", model_name)
-output_model_path= "./results/t5baseNewwords/final_Sw_whole_50"
-input_texts_path = "/home/grads/r/rohan.chaudhury/Disfluency/formatted_text/final_sw_whole/train/disfluent.txt"
-output_texts_path = "/home/grads/r/rohan.chaudhury/Disfluency/formatted_text/final_sw_whole/train/fluent.txt"
-
 import os
-os.environ["WANDB_DISABLED"] = "true"
-os.environ["CUDA_VISIBLE_DEVICES"] = "3,2,1,0" 
-
-NUM_GPU=4
-SEQUENCE_LENGTH = 512
-
 import csv
-print ("Model name: ", model_name)
-import torch_optimizer as optim
-
-
 import pickle
+from typing import Dict, List
+
 import numpy as np
+from tqdm import tqdm
+
 from sklearn.model_selection import train_test_split
+
 from tokenizers import SentencePieceBPETokenizer
 from tokenizers import Tokenizer
+import torch
+import torch_optimizer as optim
+from torch.utils.data import Dataset
 from transformers import AutoTokenizer, AutoModel
 from transformers import T5Tokenizer, T5Config, T5ForConditionalGeneration, TextDataset, DataCollatorForSeq2Seq, Seq2SeqTrainingArguments, Seq2SeqTrainer
-from torch.utils.data import Dataset
 from transformers import EarlyStoppingCallback
 from transformers import get_cosine_schedule_with_warmup
 from transformers import AdamW
-import torch
+from transformers.tokenization_utils_base import PreTrainedTokenizerBase
+from transformers.file_utils import PaddingStrategy
 
-from sklearn.metrics import precision_score, recall_score, f1_score
-from nltk.tokenize import word_tokenize
-from tqdm import tqdm
 import evaluate
+from nltk.tokenize import word_tokenize
+from sklearn.metrics import precision_score, recall_score, f1_score
 
 print ("Input texts path: ", input_texts_path)
 print ("Output texts path: ", output_texts_path)
 
+os.environ["WANDB_DISABLED"] = "true"
+os.environ["CUDA_VISIBLE_DEVICES"] = "3,2,1,0" 
+
+NUM_GPU = 4
+BATCH_SIZE = 4
+SEQUENCE_LENGTH = 512
+
+model_name = "t5-base" # "/home/grads/r/rohan.chaudhury/Disfluency/models/checkpoint-2253"
+print ("Model name:", model_name)
+output_model_path = "." # "./results/t5baseNewwords/final_Sw_whole_50"
+input_texts_path = "/home/grads/r/rohan.chaudhury/Disfluency/formatted_text/final_sw_whole/train/disfluent.txt"
+output_texts_path = "/home/grads/r/rohan.chaudhury/Disfluency/formatted_text/final_sw_whole/train/fluent.txt"
 
 # Data preparation
 input_texts = open(input_texts_path).read().split('\n')
 output_texts = open(output_texts_path).read().split('\n')
 
-
-
-all_texts= input_texts + output_texts
+all_texts = input_texts + output_texts
 all_words = []
 for i in all_texts:
     all_words.extend(i.split())
-
 all_words = list(set(all_words))
-
 print(len(all_words))
 
 
-
-lengths=[]
+lengths = []
 print(len(all_texts))
 print(len(input_texts))
 print(len(output_texts))
 
 for i in range(len(all_texts)):
     lengths.append(len(all_texts[i]))
-
 print ("Max length: ", max(lengths))
 
 length_words=[]
@@ -75,22 +67,15 @@ for i in range(len(all_texts)):
     length_words.append(len(all_texts[i].split()))
 print("Max length in words: ", max(length_words))
 
-
-
-
-#Model initialization
+# Model initialization
 tokenizer = T5Tokenizer.from_pretrained(model_name, model_max_length=SEQUENCE_LENGTH)
-
-
 
 config = T5Config.from_pretrained(model_name)
 config.model_max_length = SEQUENCE_LENGTH
 
 model = T5ForConditionalGeneration.from_pretrained(model_name, config=config)
 
-
 train_inputs, val_inputs, train_outputs, val_outputs = train_test_split(input_texts, output_texts, shuffle=True,  test_size=0.3, random_state=42)
-
 
 def add_to_input_texts(input_texts):
     input_texts = ["Remove text disfluency: " + text.strip() + " [END]" for text in input_texts]
@@ -100,16 +85,8 @@ def add_to_output_texts(input_texts):
     input_texts = [text.strip() + " [END]" for text in input_texts]
     return input_texts
 
-
-
-def remove_from_texts(input_texts):
-    input_texts = [text.replace("Remove text disfluency:", "").strip() for text in input_texts]
-    input_texts = [text.replace("[END]", "").strip() for text in input_texts]
-    return input_texts
-
 train_inputs = add_to_input_texts(train_inputs)
-val_inputs= add_to_input_texts(val_inputs)
-
+val_inputs = add_to_input_texts(val_inputs)
 
 train_outputs = add_to_output_texts(train_outputs)
 val_outputs = add_to_output_texts(val_outputs)
@@ -118,10 +95,6 @@ print(train_inputs[0])
 print(train_outputs[0])
 print(val_inputs[0])
 print(val_outputs[0])
-
-
-
-from torch.utils.data import Dataset
 
 class TranslationDataset(Dataset):
     def __init__(self, tokenizer, inputs, outputs, max_length=SEQUENCE_LENGTH):
@@ -150,11 +123,6 @@ train_dataset = TranslationDataset(tokenizer, train_inputs, train_outputs)
 val_dataset = TranslationDataset(tokenizer, val_inputs, val_outputs)
 
 
-
-from typing import Dict, List
-from transformers.tokenization_utils_base import PreTrainedTokenizerBase
-from transformers.file_utils import PaddingStrategy
-import torch
 class CustomDataCollator(DataCollatorForSeq2Seq):
     def __init__(
         self,
@@ -165,10 +133,10 @@ class CustomDataCollator(DataCollatorForSeq2Seq):
         model: T5ForConditionalGeneration = None,
     ):
         super().__init__(
-            tokenizer=tokenizer,
-            padding=padding,
-            max_length=max_length,
-            pad_to_multiple_of=pad_to_multiple_of,
+            tokenizer = tokenizer,
+            padding = padding,
+            max_length = max_length,
+            pad_to_multiple_of = pad_to_multiple_of,
         )
 
     def __call__(self, features: List[Dict[str, List[int]]]) -> Dict[str, torch.Tensor]:
@@ -177,17 +145,17 @@ class CustomDataCollator(DataCollatorForSeq2Seq):
 
         input_ids = self.tokenizer.pad(
             {"input_ids": input_ids},
-            padding=self.padding,
-            max_length=self.max_length,
-            pad_to_multiple_of=self.pad_to_multiple_of,
-            return_tensors="pt",
+            padding = self.padding,
+            max_length = self.max_length,
+            pad_to_multiple_of = self.pad_to_multiple_of,
+            return_tensors = "pt",
         )
         labels = self.tokenizer.pad(
             {"input_ids": labels},
-            padding=self.padding,
-            max_length=self.max_length,
-            pad_to_multiple_of=self.pad_to_multiple_of,
-            return_tensors="pt",
+            padding = self.padding,
+            max_length = self.max_length,
+            pad_to_multiple_of = self.pad_to_multiple_of,
+            return_tensors = "pt",
         )
 
         # Replace padding_token_id with -100 to ignore loss correctly
@@ -198,62 +166,53 @@ class CustomDataCollator(DataCollatorForSeq2Seq):
 data_collator = CustomDataCollator(tokenizer)
 
 early_stopping_callback = EarlyStoppingCallback(
-    early_stopping_patience=40,  
-    early_stopping_threshold=0.0 
+    early_stopping_patience = 40,  
+    early_stopping_threshold = 0.0 
 )
-
 
 training_args = Seq2SeqTrainingArguments(
-    output_dir=output_model_path,
-    per_device_train_batch_size=BATCH_SIZE,
-    per_device_eval_batch_size=BATCH_SIZE,
-    num_train_epochs=800,
-    save_steps=500,
-    evaluation_strategy="epoch",
-    save_strategy="epoch",
+    output_dir = output_model_path,
+    per_device_train_batch_size = BATCH_SIZE,
+    per_device_eval_batch_size = BATCH_SIZE,
+    num_train_epochs = 800,
+    save_steps = 500,
+    evaluation_strategy = "epoch",
+    save_strategy = "epoch",
     save_total_limit = 5,
-    predict_with_generate=True,
-    gradient_accumulation_steps=16//(BATCH_SIZE*NUM_GPU),
-    load_best_model_at_end=True,
-    metric_for_best_model="loss",
-    seed=42,
-    learning_rate=3e-5,
-    weight_decay=0.001,
-    report_to="none"
+    predict_with_generate = True,
+    gradient_accumulation_steps = 16//(BATCH_SIZE*NUM_GPU),
+    load_best_model_at_end = True,
+    metric_for_best_model = "loss",
+    seed = 42,
+    learning_rate = 3e-5,
+    weight_decay = 0.001,
+    report_to = "none"
 )
-
-
-
-
 
 optimizer = AdamW(model.parameters(), lr=training_args.learning_rate, weight_decay=training_args.weight_decay)
 optimizer = optim.Lookahead(optimizer)
-
 
 num_training_steps = len(train_dataset) * training_args.num_train_epochs // training_args.per_device_train_batch_size
 num_warmup_steps = int(num_training_steps * 0.1)  
 
 scheduler = get_cosine_schedule_with_warmup(
     optimizer,
-    num_warmup_steps=num_warmup_steps,
-    num_training_steps=num_training_steps
+    num_warmup_steps = num_warmup_steps,
+    num_training_steps = num_training_steps
 )
 
-
 trainer = Seq2SeqTrainer(
-    model=model,
-    args=training_args,
-    train_dataset=train_dataset,
-    eval_dataset=val_dataset,
-    data_collator=data_collator,
-    tokenizer=tokenizer,
-    callbacks=[early_stopping_callback],
-    optimizers=(optimizer, scheduler) 
+    model = model,
+    args = training_args,
+    train_dataset = train_dataset,
+    eval_dataset = val_dataset,
+    data_collator = data_collator,
+    tokenizer = tokenizer,
+    callbacks = [early_stopping_callback],
+    optimizers = (optimizer, scheduler) 
 )
 
 print ("Training started now.")
 trainer.train()
-
 trainer.evaluate()
-
 print ("Training completed.")
