38bd997d9bf08f7d2c7aced7a148e5a1062e3644
Maria Teleki <mariateleki@csce-caverlee-s1.engr.tamu.edu>
2023-09-13 16:58:46 -0500

    added spotify disfluency annotation files

--- disfluency-spotify-wikipedia/utils_model_files.py
+++ disfluency-spotify-wikipedia/utils_model_files.py
@@ -2,7 +2,7 @@ import os
 import utils_general
 
 
-def get_model_files(dataset_name="spotify", trfs=["repeats", "interjections", "false-starts", "all-3"]):
+def get_model_files(dataset_name="spotify", trfs=["repeats", "interjections", "false-starts", "all-3"], includeNegativeOne=False):
     
     # create output dir
     output_dir = os.path.join(utils_general.PATH_TO_PROJECT, dataset_name+"-model-files")
@@ -10,6 +10,24 @@ def get_model_files(dataset_name="spotify", trfs=["repeats", "interjections", "f
     
     # get the zero dir
     zero_dir = os.path.join(utils_general.PATH_TO_PROJECT, dataset_name, "0")
+    
+    # write out the -1 files
+    if includeNegativeOne:
+        
+        # clear out the model-file directory
+        utils_general.create_and_or_clear_this_dir(os.path.join(output_dir, "-1"))
+        
+        # iterate through k = 0, then append to k = 1,2,3,... for the rest of them
+        files_list = [f for f in os.listdir(zero_dir) if (f.endswith(".txt") and not f.endswith("-checkpoint.txt"))]
+        for f in files_list:
+
+            # write the zero file out to the zero csv
+            text = ""  # initialize
+            text = utils_general.read_file(os.path.join(utils_general.PATH_TO_PROJECT, dataset_name, "-1", f))
+            
+            with open(os.path.join(output_dir, "-1", "-1.txt"), mode="a") as open_file:
+                open_file.write(f + ": " + text + "\n")
+        
     
     # for the 1-10 dirs
     for t in trfs:
--- disfluency-spotify-wikipedia/utils_repair.py
+++ disfluency-spotify-wikipedia/utils_repair.py
@@ -1,12 +1,29 @@
 def get_repaired_transcript(transcript):
+    
+    sentences = transcript.split("\n")
+    
     new_transcript = []
-    for i in range(0,len(transcript)-2,2):
-        if transcript[i+1] == "E":
-            # do not add these words to the new transcript, bc they are marked as disfluent
-            pass
-        elif transcript[i+1] == "_":
-            # do add these words to the new transcript, bc they are marked as fluent
-            new_transcript.append(transcript[i])
+    for sentence in sentences:
+        
+        words = sentence.split()
+        
+        new_sentence = []
+        for i in range(0,len(words)-1,2):
             
-    new_transcript = " ".join(new_transcript)
-    return new_transcript
+            if i == 0:
+                w = words[i].capitalize()
+            elif i == len(words)-2:
+                w = words[i] + "."
+            else:
+                w = words[i]
+            
+            if words[i+1] == "E":
+                # do not add these words to the new transcript, bc they are marked as disfluent
+                pass
+            elif words[i+1] == "_":
+                # do add these words to the new transcript, bc they are marked as fluent
+                new_sentence.append(w)
+         
+        new_transcript.append(" ".join(new_sentence))
+    
+    return " ".join(new_transcript)
